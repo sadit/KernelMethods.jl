fc0a620de394e7122198c3d89ad8586b52d84fae
Eric S. Tellez <donsadit@gmail.com>
2018-01-31 18:14:50 -0600

    fixed some bugs in partition algorithms; now partition is more general and there are several particular uses in invindex, sequence, knearestreferences

--- src/kmap/kmap.jl
+++ src/kmap/kmap.jl
@@ -2,7 +2,7 @@ module KMap
 using SimilaritySearch:
     Sequential, KnnResult, search, clear!, Item
 
-export kmap, spartition, apartition, centroid
+export kmap, centroid, partition, knearestreferences, sequence
 
 include("fft.jl")
 include("criterions.jl")
@@ -10,16 +10,16 @@ include("criterions.jl")
 """
 Transforms `objects` to a new representation space induced by `(refs, dist, kernel)`
 - `refs` a list of references
-- `dkernel` a distance function (and an embedded kernel) with signature (T, T) -> Float64
+- `kernel` a kernel function (and an embedded distance) with signature (T, T) -> Float64
 """
-function kmap(objects::AbstractVector{T}, dkernel, refs::AbstractVector{T}) where {T}
+function kmap(objects::AbstractVector{T}, kernel, refs::AbstractVector{T}) where {T}
     # X = Vector{T}(length(objects))
     m = Vector{T}(length(objects))
     @inbounds for i in 1:length(objects)
         u = Vector{Float64}(length(refs))
         obj = objects[i]
         for j in 1:length(refs)
-            u[j] = dkernel(obj, refs[j])
+            u[j] = kernel(obj, refs[j])
         end
 
         m[i] = u
@@ -29,82 +29,48 @@ function kmap(objects::AbstractVector{T}, dkernel, refs::AbstractVector{T}) wher
 end
 
 """
-Computes the `k` nearest references to each object.
-The output is a matrix \$(k, |objects|)\$ of `Item` objects (SimilaritySearch), where the i-th column corresponds to the `k` nearest refences of the i-th object in `objects`.
-"""
-function spartitionitems(objects::AbstractVector{T}, dist, refs::AbstractVector{T}; k::Int=1, indexclass=Sequential) where T
-    s = Vector{Vector{Item}}(length(objects))
-    index = indexclass(refs, dist)
-    res = KnnResult(k)
-    for i in 1:length(objects)
-        clear!(res)
-        search(index, objects[i], res)
-        s[i] = collect(Item, res)
-    end
-    s
-end
-
-function spartition(objects::AbstractVector{T}, dist, refs::AbstractVector{T}; indexclass=Sequential) where T
-    s = Vector{Int}(length(objects))
-    index = indexclass(refs, dist)
-    res = KnnResult(1)
-    for i in 1:length(objects)
-        clear!(res)
-        search(index, objects[i], res)
-        s[i] = first(res).objID
-    end
-    s
-end
-
-function spartition(objects::AbstractVector{T}, dist, refs::AbstractVector{T}, k::Int; indexclass=Sequential) where T
-    s = Vector{Vector{Int}}(length(objects))
-    index = indexclass(refs, dist)
-    res = KnnResult(k)
-    for i in 1:length(objects)
-        clear!(res)
-        search(index, objects[i], res)
-        s[i] = [p.objID for p in res]
-    end
-    s
-end
-"""
 Groups items in `objects` using a nearest neighbor rule over `refs`.
-The output is an inverted index pointing from `refs` to those objects in `objects`
-having a reference as its nearest neighbor.
+The output is controlled using a callback function. The call is performed in `objects` order.
 
-- Each object can be related to more than one group \$k > 1\$ (default \$k=1\$)
+- `callback` is a function that is called for each `(objID, refItem)`
+- `objects` is the input dataset
+- `dist` a distance function \$(T, T) \\rightarrow \Re\$
+- `refs` the list of references
+- `k` specifies the number of nearest neighbors to use
+- `indexclass` specifies the kind of index to be used, a function receiving `(refs, dist)` as arguments,
+    and returning the new metric index
+
+Please note that each object can be related to more than one group \$k > 1\$ (default \$k=1\$)
 """
-function apartitionitems(objects::Vector{T}, dist, refs::Vector{T}; k::Int=1, indexclass=Sequential) where T
-    π = [Vector{Item}() for i in 1:length(refs)] 
+function partition(callback::Function, objects::AbstractVector{T}, dist, refs::AbstractVector{T}; k::Int=1, indexclass=Sequential) where T
     index = indexclass(refs, dist)
     res = KnnResult(k)
     for i in 1:length(objects)
         clear!(res)
         search(index, objects[i], res)
         for p in res
-            push!(π[p.objID], p)
+            callback(i, p)
         end
     end
-
-    π
 end
 
-function apartition(objects::Vector{T}, dist, refs::Vector{T}; k::Int=1, indexclass=Sequential) where T
+function invindex(objects::AbstractVector{T}, dist, refs::AbstractVector{T}; k::Int=1, indexclass=Sequential) where T
     π = [Vector{Int}() for i in 1:length(refs)]
-    index = indexclass(refs, dist)
-    res = KnnResult(k)
-    for i in 1:length(objects)
-        clear!(res)
-        search(index, objects[i], res)
-        for p in res
-            push!(π[p.objID], i)
-        end
-    end
-
+    partition((i, p) -> push!(π[p.objID], i), objects, dist, refs, k=k, indexclass=indexclass)
     π
 end
 
+function sequence(objects::AbstractVector{T}, dist, refs::AbstractVector{T}; indexclass=Sequential) where T
+    s = Vector{Int}(length(objects))
+    partition((i, p) -> begin s[i] = p.objID end, objects, dist, refs, indexclass=indexclass)
+    s
+end
 
+function knearestreferences(objects::AbstractVector{T}, dist, refs::AbstractVector{T}; indexclass=Sequential) where T
+    s = Vector{Vector{Int}}(length(objects))
+    partition((i, p) -> s[i] = [p.objID for p in res], objects, dist, refs, indexclass=indexclass)
+    s
+end
 """
 Computes the centroid of the list of objects
 
